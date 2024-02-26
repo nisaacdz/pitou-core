28146c40a5c3e87edafbccb3e35bddb4af6cb481
nisaacdz <nisaacdz@gmail.com>
2024-02-26 23:17:09 +0000

    removed bugs

--- src/search/fxns.rs
+++ src/search/fxns.rs
@@ -5,20 +5,7 @@ use crate::PitouFile;
 use super::SearchOptions;
 
 mod stream {
-    use std::{collections::LinkedList, sync::OnceLock};
-    use static_init::dynamic;
-    use crate::PitouFile;
-
-    #[dynamic]
-    static mut STREAM: Option<LinkedList<PitouFile>> = None;
-
-    pub fn read() -> Option<LinkedList<PitouFile>> {
-        match &STREAM.write() {
-            Some(v) => ,
-            None => None,
-        }
-    }
-
+    
 }
 
 
